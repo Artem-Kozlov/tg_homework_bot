0ad89d9b53b81310cc395f69fc4812d3bf5992df
artem <artem.kozlov-2001@mail.ru>
2024-12-04 00:45:09 +0300

    /start

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+from aiogram.filters import Command
 import asyncio
+
 
 TOKEN = "7169731413:AAEyPHs1ZJY4CaoDeNbGc3Auwxc1r_Hta6w"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+# Обработчик команды /start
+@dp.message(Command("start"))
+async def start_command(message: Message):
+    await message.answer(f"Привет, {message.from_user.first_name}! 👋\n"
+                         "Я ваш бот. Чем могу помочь?")
 
 @dp.message(F.text)
 async def echo_handler(message: Message):
